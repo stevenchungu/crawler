e8f3f889a9be38f68799ccdf477697ac038fedaa
stevenchungu <bed-com-07-18@unima.ac.mw>
2023-06-22 15:09:52 +0200

    second commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,31 +1,11 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { CrawlingModule } from './crawling.module';
-
-
 
 @Module({
 
-  imports: [TypeOrmModule.forRoot({
-    type: 'mysql',
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT, 10),
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    autoLoadEntities: false,
-    synchronize: true,
-    entities: [__dirname + '/**/*.entity{.ts,.js}'],
-    driver: require('mysql2'),
-
-   
-  }), 
+  imports: [
   CrawlingModule
- 
   ],
-
-
-
 })
 export class AppModule { }
 
@@ -35,6 +15,18 @@ export class AppModule { }
 //     username: 'root',
 //     password: '',
 //     database: 'tsogolo',
+//     autoLoadEntities: false,
+//     synchronize: true,
+//     entities: [__dirname + '/**/*.entity{.ts,.js}'],
+//     driver: require('mysql2'),
+
+
+
+// host: process.env.DB_HOST,
+//     port: parseInt(process.env.DB_PORT, 10),
+//     username: process.env.DB_USERNAME,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_DATABASE,
 //     autoLoadEntities: false,
 //     synchronize: true,
 //     entities: [__dirname + '/**/*.entity{.ts,.js}'],
